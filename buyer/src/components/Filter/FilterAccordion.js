3951148710d4551b89070bf14837f9a7ec274174
Deveshi Singh <deveshi@Deveshis-Laptop.local>
2024-08-13 12:55:53 +0530

    sliders

--- buyer/src/components/Filter/FilterAccordion.js
+++ buyer/src/components/Filter/FilterAccordion.js
@@ -1,34 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './Accordion';
-import { Button } from 'react-bootstrap';
+import { Button, Form, InputGroup, Range } from 'react-bootstrap';
 import CheckState from './CheckState';
 
 import './Accordion.css';
 
 const FilterAccordion = () => {
+    const [price, setPrice] = useState(9999);
+    const [distance, setDistance] = useState(1000);
+
+    const handlePriceChange = (event) => {
+        setPrice(event.target.value);
+    };
+
+    const handleDistanceChange = (event) => {
+        setDistance(event.target.value);
+    };
+
     return (
         <div>
             <div className="clear-filter-div">
                 <Button variant="outline-success">CLEAR ALL</Button>
             </div>
-            <Accordion title="Sort">
-                <CheckState />
-            </Accordion>
-            <Accordion title="Category">
-                <span className="accordion-text">aaaaaa</span>
-            </Accordion>
-            <Accordion title="Availability">
-                <span className="accordion-text">aaaaaa</span>
-            </Accordion>
             <Accordion title="Price">
-                <span className="accordion-text">bbbbbb</span>
+                <Form>
+                    <Form.Group controlId="formPriceRange">
+                        <Form.Label>Price (Up to {price} rupees)</Form.Label>
+                        <InputGroup>
+                            <Form.Control
+                                type="range"
+                                min="1"
+                                max="9999"
+                                value={price}
+                                onChange={handlePriceChange}
+                            />
+                        </InputGroup>
+                    </Form.Group>
+                </Form>
             </Accordion>
             <Accordion title="Location">
-                <span className="accordion-text">cccccc</span>
+                <Form>
+                    <Form.Group controlId="formDistanceRange">
+                        <Form.Label>Distance (Up to {distance} km)</Form.Label>
+                        <InputGroup>
+                            <Form.Control
+                                type="range"
+                                min="5"
+                                max="1000"
+                                value={distance}
+                                onChange={handleDistanceChange}
+                            />
+                        </InputGroup>
+                    </Form.Group>
+                </Form>
             </Accordion>
         </div>
-    )
+    );
 };
-
 
 export default FilterAccordion;
