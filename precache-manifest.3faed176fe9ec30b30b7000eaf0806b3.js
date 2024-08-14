04fb4f6ee385e4f9a71f4ac754c012146e09e80b
Deveshi Singh <deveshi@Deveshis-Laptop.local>
2024-08-14 22:37:54 +0530

    Updates

---
+++ precache-manifest.3faed176fe9ec30b30b7000eaf0806b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bd6beb087cc519e0d09cdb5830b329a",
+    "url": "/agri-hackathon/index.html"
+  },
+  {
+    "revision": "937b349d03d1b5ff0921",
+    "url": "/agri-hackathon/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "a538ec1d8b3004b93bd9",
+    "url": "/agri-hackathon/static/css/main.97b6c4b1.chunk.css"
+  },
+  {
+    "revision": "937b349d03d1b5ff0921",
+    "url": "/agri-hackathon/static/js/2.d43dac4a.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/agri-hackathon/static/js/2.d43dac4a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a538ec1d8b3004b93bd9",
+    "url": "/agri-hackathon/static/js/main.912a25e8.chunk.js"
+  },
+  {
+    "revision": "91aa20c248c00b77970a",
+    "url": "/agri-hackathon/static/js/runtime-main.f738235c.js"
+  }
+]);
