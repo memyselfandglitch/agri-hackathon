1267bfdb33467f39d40d4849c047287782e7cabc
vaishnavi <pawarvaishnavi196@gmail.com>
2024-08-14 17:25:38 +0530

    error resolved

--- farmer/src/App.js
+++ farmer/src/App.js
@@ -80,31 +80,6 @@ const App = () => {
         <MainNavigation />
         <main>{routes}</main>
         <Footer />
-        <df-messenger
-          project-id="maximal-run-432015-n9"
-          agent-id="7b4ff456-1303-40cb-819d-6beb3da56ba0"
-          language-code="en"
-          max-query-length="-1"
-        >
-          <df-messenger-chat chat-title="Deveshi Singh"></df-messenger-chat>
-        </df-messenger>
-        <style>
-          {`
-            df-messenger {
-              z-index: 999;
-              position: fixed;
-              --df-messenger-font-color: #000;
-              --df-messenger-font-family: Google Sans;
-              --df-messenger-chat-background: #f3f6fc;
-              --df-messenger-message-user-background: #d3e3fd;
-              --df-messenger-message-bot-background: #fff;
-              bottom: 0;
-              right: 0;
-              top: 0;
-              width: 350px;
-            }
-          `}
-        </style>
       </Router>
     </AuthContext.Provider>
   );
